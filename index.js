d0076a02dc7539888d609f66542a75e380cd14bb
Maaz Ahmad <maaz@example.com>
2023-10-16 04:19:56 -0700

    capital T of default todolist title

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/" , async(req , res)=>{
 
     if (allItems.length === 0) {
         const item1 = new NewList({
-            name:"todo list",
+            name:"Todo list",
             listData:[
                 {
                     name:"...",
