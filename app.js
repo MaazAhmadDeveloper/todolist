6d5b833ccf726fbb0b6eda2a81e3ba126ee2d762
Maaz Ahmad <maaz@example.com>
2023-10-16 04:50:16 -0700

    set the code for live hosting

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import NewList from "./database/models/newList.js";
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"));
